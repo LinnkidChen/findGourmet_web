63c55576ace3026aad82f76a1586b707dcd0aa96
Linnkid_CHEN <56332312+LinnkidChen@users.noreply.github.com>
2022-11-07 07:31:12 +0800

    update venv & init project structure

---
+++ findGourmet/backend/fingGourmet.py
@@ -0,0 +1,16 @@
+#please set environment variable FLASK_APP=findGourmet.py
+
+import os
+import click
+from flask_migrate import Migrate
+from app import create_app, db
+from app.models import User, Follow, Role, Permission, Post, Comment
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
+# save time to set User as FindG.user when operating database commandshell
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Follow=Follow, Role=Role,
+                Permission=Permission, Post=Post, Comment=Comment)
