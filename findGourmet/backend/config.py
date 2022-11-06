e133703127775ee357db8a6a626bd26a1f5e1a56
Linnkid_CHEN <56332312+LinnkidChen@users.noreply.github.com>
2022-11-07 06:46:19 +0800

    init config.py

--- findGourmet/backend/config.py
+++ findGourmet/backend/config.py
@@ -12,37 +12,29 @@ class Config:
     # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     # FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     # FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FINDG_ADMIN = os.environ.get('FINDG_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASKY_POSTS_PER_PAGE = 20
-    FLASKY_FOLLOWERS_PER_PAGE = 50
-    FLASKY_COMMENTS_PER_PAGE = 30
+    FINDG_POSTS_PER_PAGE = 20
+    FINDG_FOLLOWERS_PER_PAGE = 50
+    FINDG_COMMENTS_PER_PAGE = 30
 
     @staticmethod
     def init_app(app):
         pass
-
-
+    
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+        
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
-class TestingConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'sqlite://'
-
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-
-
 config = {
     'development': DevelopmentConfig,
-    'testing': TestingConfig,
+
     'production': ProductionConfig,
 
     'default': DevelopmentConfig
