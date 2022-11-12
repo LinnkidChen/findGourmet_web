3438c06ca6eeb964079bcb24f05885c7cc32e005
Linnkid_CHEN <56332312+LinnkidChen@users.noreply.github.com>
2022-11-12 19:51:46 +0800

    一点问题标注

--- findGourmet/backend/app/api/user.py
+++ findGourmet/backend/app/api/user.py
@@ -153,46 +153,49 @@ def user_reg():
     else:
         return bad_request("no username or password included in request")
 
-@api.route("user/modifyMessage", methods=['POST'])
+
+@api.route("user/modifyMessage", methods=["POST"])
 @auth.login_required
 def modifyMessage():
     modify_dict = request.get_json()
-    user = User.query.filter_by(id=modify_dict['id']).first()
-    res_raw = {"username":user.username}
-    if len(modify_dict['phoneNumber']) == 11 and modify_dict['phoneNumber'].isdigit():
-        user.phoneNumber = modify_dict['phoneNumber']
+    user = User.query.filter_by(id=modify_dict["id"]).first()
+    res_raw = {"username": user.username}
+    if len(modify_dict["phoneNumber"]) == 11 and modify_dict["phoneNumber"].isdigit():
+        user.phoneNumber = modify_dict["phoneNumber"]
         db.session.commit()
-        res_raw['phoneNumber'] = 'modify success'
+        res_raw["phoneNumber"] = "modify success"
     else:
-        res_raw['phoneNumber'] = 'invalid phoneNumber'
+        res_raw["phoneNumber"] = "invalid phoneNumber"
 
-    if modify_dict['introduce'] != '' and modify_dict['introduce'] is not None:
-        user.introduce = modify_dict['introduce']
+    if modify_dict["introduce"] != "" and modify_dict["introduce"] is not None:
+        user.introduce = modify_dict["introduce"]
         db.session.commit()
-        res_raw['introduce'] = 'modify sucess'
+        res_raw["introduce"] = "modify sucess"
     else:
-        res_raw['introduce'] = 'invalid introduce'
+        res_raw["introduce"] = "invalid introduce"
 
-    if len(modify_dict['password']) >= 6 and password_valid(modify_dict['password']):
-        user.password = modify_dict['password']
+    if len(modify_dict["password"]) >= 6 and password_valid(modify_dict["password"]):
+        user.password = modify_dict["password"]
         db.session.commit()
-        res_raw['password'] = 'modify success'
+        res_raw["password"] = "modify success"
     else:
-        res_raw['password'] = 'invalid password'
+        res_raw["password"] = "invalid password"
 
     response = jsonify(res_raw)
     response.status_code = 200
     return response
+
 
 def password_valid(password):
     digit = 0
     for p in password:
         if p.isdigit():
             digit += 1
-    if digit>=2 and not password.islower() and not password.isupper():
+    if digit >= 2 and not password.islower() and not password.isupper():
         return True
     else:
         return False
+
 
 @api.route("/user/pageFindAll/<int:index>")
 def fine_all_users():
@@ -222,10 +225,11 @@ def query_user():
 # [<User 'john'>, <User 'marry'>]
 # >>>
 
+
 @api.route("/user/getCitys")
 def getCitys():
     users = User.query.all()
-    i = 1
+    i = 1  # TODO 实现逻辑有点问题 把城市加在config里
     citys = []
     city_set = set()
     for user in users:
