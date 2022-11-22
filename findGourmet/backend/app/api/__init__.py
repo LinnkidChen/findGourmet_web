6db00275827a81312df78bae7408a81f14149555
Linnkid_CHEN <56332312+LinnkidChen@users.noreply.github.com>
2022-11-22 14:59:45 +0800

    将部分api转移到post.py中

--- findGourmet/backend/app/api/__init__.py
+++ findGourmet/backend/app/api/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 
-api = Blueprint('api', __name__)
+api = Blueprint("api", __name__)
 
 # from . import authentication, posts, users, comments, errors
 from . import user
+from . import posts
